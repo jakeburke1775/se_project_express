6901f8af29321fe6ee10b6e0a44aff8a7bee4d83
Jake B <jakeburke1775@gmail.com>
2025-10-04 20:27:32 -0500

    complete and push crud operations

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -2,13 +2,13 @@ const ClothingItem = require("../models/clothingItem");
 const { CREATED, SERVER_ERROR, SUCCESS } = require("../utils/errors");
 
 const createItem = (req, res) => {
-  console.log("Request body:", req.body); // Add this line to debug
+  // console.log("Request body:", req.body); // Add this line to debug
 
   const { name, weather, imageUrl, owner } = req.body;
 
-  ClothingItem.create({ name, weather, imageUrl, owner })
+  ClothingItem.create({ name, weather, imageUrl, owner }) // changed from URL to Url
     .then((item) => {
-      console.log(item);
+      // console.log(item);  // Optional: log the created item for debugging
       res.status(CREATED).send({ data: item });
     })
     .catch((err) => {
@@ -21,7 +21,7 @@ const getItems = (req, res) => {
   ClothingItem.find({})
     .then((items) => res.status(SUCCESS).send(items))
     .catch((err) => {
-      console.error(err);
+      // console.error(err); // Uncomment this line to log the error for debugging
       res.status(SERVER_ERROR).send({ message: "Error from getItems", err });
     });
 };
@@ -36,21 +36,21 @@ const updateItem = (req, res) => {
       res.status(SUCCESS).send({ data: item });
     })
     .catch((err) => {
-      console.error(err);
+      // console.error(err); // Uncomment this line to log the error for debugging
       res.status(SERVER_ERROR).send({ message: "Error from updateItem", err });
     });
 };
 
 const deleteItem = (req, res) => {
   const { itemId } = req.params;
-  console.log("Deleting item with ID:", itemId); // Debugging line
+  // console.log("Deleting item with ID:", itemId); // Debugging line
   ClothingItem.findByIdAndDelete(itemId)
     .orFail()
     .then((item) => {
       res.status(SUCCESS).send({ data: item });
     })
     .catch((err) => {
-      console.error(err);
+      // console.error(err); // Uncomment this line to log the error for debugging
       res.status(SERVER_ERROR).send({ message: "Error from deleteItem", err });
     });
 };
